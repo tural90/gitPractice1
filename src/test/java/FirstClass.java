f60c6c888384e336e5f1a9af25e87cc3f7e90248
turalfataliyev <turalfetali@yahoo.com>
2019-12-12 11:34:00 -0500

    second

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -5,5 +5,7 @@ public class FirstClass {
         System.out.println("I am working until 5.10");
         System.out.println("this is a new line");
         System.out.println("this is from develop branch");
+        System.out.println("complete new line");
+        System.out.println("last line");
     }
 }
