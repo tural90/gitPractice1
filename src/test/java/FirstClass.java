b4cca80ba6c4256cad144b8aa0e9750126a22ce6
turalfataliyev <turalfetali@yahoo.com>
2019-12-12 10:44:12 -0500

    changed the develop branch

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -3,5 +3,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hi world");
         System.out.println("I am working until 5.10");
+        System.out.println("this is a new line");
+        System.out.println("this is from develop branch");
     }
 }
