dcc634c82d5b89a3a1f2306c11fd9d420b7a6757
turalfataliyev <turalfetali@yahoo.com>
2019-12-12 11:40:05 -0500

    update

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -3,5 +3,6 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hi world");
         System.out.println("I am working until 5.10");
+        System.out.println("new line");
     }
 }
