544a9c6a86213488f9a7a8f5f03ba74f78d1eda3
turalfataliyev <turalfetali@yahoo.com>
2019-12-12 10:36:12 -0500

    ‘first_commit’

---
+++ src/test/java/FirstClass.java
@@ -0,0 +1,7 @@
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hi world");
+        System.out.println("I am working until 5.10");
+    }
+}
---
+++ src/test/java/SecondClass.java
@@ -0,0 +1,8 @@
+public class SecondClass {
+    public static void main(String[] args) {
+        String myname = "Humayra";
+        System.out.println(myname);
+        System.out.println("this  is another line");
+        System.out.println("this  is another line");
+    }
+}
