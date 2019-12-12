7f91652a92a3f5db7c080c567cb74fa5ce4b29d7
turalfataliyev <turalfetali@yahoo.com>
2019-12-12 11:49:49 -0500

    interesant

--- src/test/java/FirstClass.java
+++ src/test/java/FirstClass.java
@@ -4,5 +4,6 @@ public class FirstClass {
         System.out.println("hi world");
         System.out.println("I am working until 5.10");
         System.out.println("new line");
+        System.out.println("very new line");
     }
 }
